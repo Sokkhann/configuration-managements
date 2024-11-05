e7dedb8f4468d53b7cf69c9524123eba8e8c5668
sk <mrrkhann9u@email.com>
2024-11-05 09:05:22 +0700

    add eureka service for service discovery and registry

--- core sub-domain/restaurant/src/main/java/com/sk/microservice/restaurant/RestaurantMsApplication.java
+++ core sub-domain/restaurant/src/main/java/com/sk/microservice/restaurant/RestaurantMsApplication.java
@@ -2,7 +2,9 @@ package com.sk.microservice.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RestaurantMsApplication {
 
